253a91fa1f7cde9d3d4686c655dd4775ce770493
edusolis <edusolis@expediagroup.com>
2019-11-14 11:23:38 -0600

    Change the writing of raw so it just dumps the value of the secret without the secret prefix

--- examples/secrets-init/source/main.py
+++ examples/secrets-init/source/main.py
@@ -6,7 +6,6 @@ import toml
 from unflatten import unflatten
 from jproperties import Properties
 from ec2_metadata import ec2_metadata
-client = boto3.client('secretsmanager', region_name=ec2_metadata.region)
 
 if (len(sys.argv) == 1):
     print("No secrets to be mounted, exiting")
@@ -15,6 +14,14 @@ if (len(sys.argv) == 1):
 secret_names = sys.argv[1].split(",")
 out_directory = sys.argv[2]
 file_type = sys.argv[3]
+
+#for development or debbug get allow to pass the region via CLI
+if len(sys.argv) >= 5 :
+  region_name = sys.argv[4]
+else:
+   region_name = ec2_metadata.region
+
+client = boto3.client('secretsmanager', region_name=region_name)
 
 values = {secret["Name"].replace('/', '.'): secret["SecretString"] for secret in map(
     lambda name: client.get_secret_value(SecretId=name), secret_names)}
@@ -27,8 +34,6 @@ elif file_type == "json":
 elif file_type == "toml":
     toml.dump(unflatten(values), open(file_name, 'w'))
 else:
-    properties = Properties()
-    properties.properties = values
-
-    with open(file_name, "wb") as out_file:
-        properties.store(out_file, strict=True)
+    with open(file_name, "w") as out_file:
+      for v in values.values():
+        out_file.write('{}\n'.format(v))
